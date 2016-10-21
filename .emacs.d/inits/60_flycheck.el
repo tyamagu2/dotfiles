d7a93aa7e3db3eec4a597d8f78bf5ffde4663c5a
tyamagu2 <tyamagu2@gmail.com>
2016-10-22 00:33:10 +0900

    Update flycheck hook

--- .emacs.d/inits/60_flycheck.el
+++ .emacs.d/inits/60_flycheck.el
@@ -1,3 +1,2 @@
-(eval-after-load 'flycheck
-    '(custom-set-variables
-         '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages)))
+(with-eval-after-load 'flycheck
+  (flycheck-pos-tip-mode)
